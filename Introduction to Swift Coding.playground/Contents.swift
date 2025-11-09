f6d90e6bbe06ecccbb3eb63d5c540a173b3b340c
alexdaluz <alexdaluz@me.com>
2025-11-09 15:45:34 -0600

    Add files via upload

---
+++ Introduction to Swift Coding.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var greeting:String = "Hello, playground"
+greeting = "I can write Swift!"
+print(greeting)
+
+let myInt:Int = 5
+print(myInt)
+
+var myDouble:Double = 0.5
+print(myDouble)
+
+var myBool:Bool = true
+print(myBool)
